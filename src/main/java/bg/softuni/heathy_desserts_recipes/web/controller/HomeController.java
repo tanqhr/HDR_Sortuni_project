62c90571f26b532cea8f100e8055d8ceb44082d3
tanqhr <tanqhr@abv.bg>
2025-01-26 00:42:58 +0200

    HDR_softuni_project

--- src/main/java/bg/softuni/heathy_desserts_recipes/web/controller/HomeController.java
+++ src/main/java/bg/softuni/heathy_desserts_recipes/web/controller/HomeController.java
@@ -1,15 +1,34 @@
 package bg.softuni.heathy_desserts_recipes.web.controller;
 
+import bg.softuni.heathy_desserts_recipes.service.WeatherService;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class HomeController {
+    private final WeatherService weatherService;
+
+    public HomeController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 
     @GetMapping("/")
-    public String getHome () {
+    public String getHome(Model model) {
+        JsonNode weatherData = weatherService.getWeather();
+        model.addAttribute("temperature", weatherData.path("main").path("temp").asText());
+        model.addAttribute("wind", weatherData.path("wind").path("speed").asText());
+
+        String rain = "без валеж";
+        if (weatherData.has("rain") && weatherData.path("rain").has("1h")) {
+            rain = weatherData.path("rain").path("1h").asText();
+        }
+        model.addAttribute("rain", rain);
 
         return "home";
     }
+
 }
