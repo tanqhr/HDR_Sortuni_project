10b62e4074c36e002c503b7a07506e15c2dcbd79
tanqhr <tanqhr@abv.bg>
2025-01-26 19:29:44 +0200

    adding contact

--- src/main/java/bg/softuni/heathy_desserts_recipes/web/controller/HomeController.java
+++ src/main/java/bg/softuni/heathy_desserts_recipes/web/controller/HomeController.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -30,5 +30,9 @@ public class HomeController {
 
         return "home";
     }
+    @GetMapping("/contact")
+    public String getContact(){
+        return "contact";
 
+}
 }
